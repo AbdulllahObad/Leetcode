b341fcd72fde7fdf256b3d28a54eb626870ec595
AbdulllahObad <abdullah.s.obad@gmail.com>
2022-08-25 05:50:23 +0200

    Time: 531 ms (78.18%), Space: 26.1 MB (5.18%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,13 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         
-        dictonary ={}
-        for i in range(len(nums)):  
-            if dictonary.get(nums[i]) is not None:
-                 return True
-            else :
-                dictonary[nums[i]] = 2
-        return False
+        nums = sorted(nums)
+        for i in range(1,len(nums)):
+            if nums[i] ==nums[i-1]:
+                return True
+        return False 
             
                
                 
