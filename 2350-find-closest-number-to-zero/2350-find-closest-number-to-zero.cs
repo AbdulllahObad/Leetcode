b77ce91031f3d7c85bfe210d4cb18435f5b24226
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-01-23 12:02:24 -0700

    Time: 2 ms (99.7%), Space: 47 MB (58.18%) - LeetHub

--- 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cs
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cs
@@ -1,23 +1,15 @@
 public class Solution {
     public int FindClosestNumber(int[] nums) {
-      int closest = nums[0]; // set the variable to the fst ele of the arr
-      // Return the smallest abs value
-        foreach(int num in nums){
-            // If the current num is less than the closet to zero
+      int closest = nums[0];// set the first element to be closest
+
+      foreach(int num in nums){
             if(Math.Abs(num) < Math.Abs(closest)){
-                closest = num; 
-            }else{
-                 if(Math.Abs(num) == Math.Abs(closest) &&
-                 num >0){
-                    closest = num;
-
-                 }
-
+                closest = num;
+            }else if(Math.Abs(num) == Math.Abs(closest) && num > 0){
+                closest = num;
             }
-        }
-        
-        return closest;
-  
+      }
 
-    }
+      return closest;
+}
 }
