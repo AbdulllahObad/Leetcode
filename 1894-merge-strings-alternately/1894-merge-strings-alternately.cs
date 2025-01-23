24ffdf486a8231811e53c6dd11646a44bd61e57d
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-01-23 13:01:54 -0700

    Time: 66 ms (48.44%), Space: 41.2 MB (14.65%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
@@ -5,18 +5,26 @@ public class Solution {
     int size3 = Math.Max(size1, size2);
     string word3 = "";
     for(int i = 0; i<size3; i++){
-        if(i >= size1){     
-            char letter_W2 = word2[i];
-            word3 += letter_W2;
-        }else if (i >= size2){
-            char letter_W1 = word1[i];
-            word3 += letter_W1;
-        }else {
-            char letter_W1 = word1[i];
-            char letter_W2 = word2[i];
-                word3 += letter_W1;
-                word3 += letter_W2;
+        // if the current iteration is less than the first word, add it 
+        if(i < size1){
+            word3 += word1[i];
         }
+        // if the current iteration is less than the second word, add it 
+        if(i < size2){
+            word3 += word2[i];
+        }
+        // if(i >= size1){     
+        //     char letter_W2 = word2[i];
+        //     word3 += letter_W2;
+        // }else if (i >= size2){
+        //     char letter_W1 = word1[i];
+        //     word3 += letter_W1;
+        // }else {
+        //     char letter_W1 = word1[i];
+        //     char letter_W2 = word2[i];
+        //         word3 += letter_W1;
+        //         word3 += letter_W2;
+        // }
     }  
 
     return word3;
