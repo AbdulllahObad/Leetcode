f95eb25240b02fbd9aaea9300fcb28605b8293b2
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-01-23 12:57:27 -0700

    Time: 61 ms (75.83%), Space: 41.3 MB (7.41%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
@@ -1,32 +1,24 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-
-        int word1_l = word1.Length;
-        int word2_l = word2.Length;
-
-        int merged_word_length = Math.Max(word1_l,word2_l);
-
-        //string merged_word = "";
-        StringBuilder Merged_word = new StringBuilder();
-
-        for(int i= 0; i<merged_word_length; i++){
-             //if we still have letters to merge in word1
-             if(i< word1_l ){
-             //Place the ith letter in word1
-              char word1_letter = word1[i];
-             // Merge the letters
-             Merged_word.Append(word1_letter);
-            }
-             //if we still have letters to merge in word2
-             if(i< word2_l ){    
-            //Get the ith letter in word2
-             char word2_letter = word2[i];
-             Merged_word.Append(word2_letter);
-            }
+    int size1 = word1.Length;
+    int size2 = word2.Length;
+    int size3 = Math.Max(size1, size2);
+    string word3 = "";
+    for(int i = 0; i<size3; i++){
+        if(i >= size1){     
+            char letter_W2 = word2[i];
+            word3 += letter_W2;
+        }else if (i >= size2){
+            char letter_W1 = word1[i];
+            word3 += letter_W1;
+        }else {
+            char letter_W1 = word1[i];
+            char letter_W2 = word2[i];
+                word3 += letter_W1;
+                word3 += letter_W2;
         }
-        string result = Merged_word.ToString();
+    }  
 
-        return result;
-        
+    return word3;
     }
 }
