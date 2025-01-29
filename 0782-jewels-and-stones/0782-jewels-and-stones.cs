67f1d9046b504349c0488f352a9a960cb0a7f847
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-01-28 21:42:59 -0700

    Time: 0 ms (100%), Space: 40.3 MB (5.24%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.cs
+++ 0782-jewels-and-stones/0782-jewels-and-stones.cs
@@ -2,16 +2,20 @@ public class Solution {
     public int NumJewelsInStones(string jewels, string stones) {
         //Idea1: 
         //Brute force: Try every char in jewels and count how many time it apears in stones. 
+        //Idea2: HashSet: loop through the stones and check if each one one the hash.
+
+        HashSet<char> jewelsSet = new HashSet<char>(jewels);
         int count = 0;
-        foreach(char j in jewels){
-            foreach(char s in stones){
-                if(j == s){
-                    count++;
-                }
+        foreach(char s in stones){
+            if(jewelsSet.Contains(s)){
+                count++;
             }
+            // foreach(char s in stones){
+            //     if(j == s){
+            //         count++;
+            //     }
+            // }
         }
-        return count;
-  
-        
+        return count;    
     }
 }
