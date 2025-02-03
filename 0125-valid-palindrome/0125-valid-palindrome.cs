bd82713bd6625434fc78dd0c31b414ebb9ab264e
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-02-02 22:00:50 -0700

    Time: 19 ms (56.19%), Space: 45.1 MB (56.32%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -1,19 +1,38 @@
 public class Solution {
     public bool IsPalindrome(string s) {
 
-        string trimmed = new String(s.ToLower().Where(char.IsLetterOrDigit).ToArray());
-        int right = 0;
-        int left = trimmed.Length -1;
-        while( right <= left){
+        // string trimmed = new String(s.ToLower().Where(char.IsLetterOrDigit).ToArray());
+        // int right = 0;
+        // int left = trimmed.Length -1;
+        // while( right <= left){
 
-            if(trimmed[right] != trimmed[left]){
-                return false;
-            }else {
-                right++;
-                left--;
+        //     if(trimmed[right] != trimmed[left]){
+        //         return false;
+        //     }else {
+        //         right++;
+        //         left--;
+        //     }
+        // }
+
+
+        int left = 0;
+        int right = s.Length -1;
+        while(left < right ){
+            if(Char.IsLetterOrDigit(s[right]) && Char.IsLetterOrDigit(s[left])){
+                if(Char.ToLower(s[right]) != Char.ToLower(s[left])){
+                    return false;
+                }else{
+                    right--;
+                    left++;
+                }
+            }    
+            if(!Char.IsLetterOrDigit(s[right])){
+                right--;
+            }
+             if(!Char.IsLetterOrDigit(s[left])){
+                left++;
             }
         }
-
         return true;
 
         
