478a02011c96fb22905a6d13ff5e57ef7dee0026
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-02-02 19:46:52 -0700

    Time: 2 ms (64.5%), Space: 64.5 MB (38.67%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cs
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cs
@@ -5,13 +5,21 @@ public class Solution {
         // for(int i=0;i<nums.Length;i++){
         //     numsArry[i] = nums[i]*nums[i];
         // }
-        int[] numsArray = nums.Select(n => n*n).ToArray();
-        Array.Sort(numsArray);
- 
-
-
-
-        return numsArray;
-
+        // int[] numsArray = nums.Select(n => n*n).ToArray();
+        // Array.Sort(numsArray);
+        int right = 0;
+        int left = nums.Length -1;
+        List<int> res = new List<int>();
+        while (right <= left){
+            if(Math.Abs(nums[right]) < Math.Abs(nums[left])){
+                res.Add(nums[left]*nums[left]);
+                left--;
+            }else{
+                res.Add(nums[right]*nums[right]);
+                right++;
+                }
+        } 
+        res.Reverse();
+        return res.ToArray();
     }
 }
