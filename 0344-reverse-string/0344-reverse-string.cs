c167504e80ad0e1df15701c74cc33b1b4dfac76b
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-04-02 14:19:36 -0600

    Time: 0 ms (100%), Space: 64.7 MB (81.54%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cs
+++ 0344-reverse-string/0344-reverse-string.cs
@@ -1,17 +1,16 @@
 public class Solution {
     public void ReverseString(char[] s) {
+        // ["h","e","l","l","o"]
+        int size = s.Length;
+        for(int i = 0; i<size/2;i++){
+            char start = s[i];
+            char end = s[(s.Length -1) - i];
+            s[i] = end;
+            s[(s.Length -1) - i] = start;   
+        }
 
-        // int right = 0;
-        // int left = s.Length -1;
-        // while(right <= left){
-        //     char char_right = s[right];
-        //     char char_left = s[left];
-        //     s[right] = char_left;
-        //     s[left] =char_right; 
-        //     right++;
-        //     left--;
-        // }    
 
-        Array.Reverse(s);
+
+        
     }
 }
