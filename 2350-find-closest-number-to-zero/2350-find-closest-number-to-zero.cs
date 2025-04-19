b1415e3f8140d8729aa1e9598f46d04a3ab76b35
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-04-18 23:35:52 -0600

    Time: 3 ms (58.15%), Space: 47.6 MB (16.31%) - LeetHub

--- 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cs
+++ 2350-find-closest-number-to-zero/2350-find-closest-number-to-zero.cs
@@ -1,15 +1,26 @@
+// Thoughts...
+// Thoughts...
+// 1. Iterate through each element in the array.
+// 2. Assume the first element is the closest to zero.
+// 3. For each number in the loop:
+//    3.1 Compare its absolute value with the current closest number.
+//        - If it's smaller, update the closest number.
+//    3.2 If absolute values are equal, prefer the positive number.
+//        - If the current number is positive and equal in absolute value, update it.
+
+
+
 public class Solution {
     public int FindClosestNumber(int[] nums) {
       int closest = nums[0];// set the first element to be closest
-
       foreach(int num in nums){
-            if(Math.Abs(num) < Math.Abs(closest)){
-                closest = num;
-            }else if(Math.Abs(num) == Math.Abs(closest) && num > 0){
-                closest = num;
-            }
+        // if abs(num) is smaller than abs(closest), update it 
+        if(Math.Abs(num) < Math.Abs(closest)){
+            closest = num;
+        }else if(Math.Abs(num)==Math.Abs(closest) && num> 0){
+            closest = num;
+        }
       }
-
       return closest;
 }
 }
