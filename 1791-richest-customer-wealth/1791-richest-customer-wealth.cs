bf5b956c89e0b913368e46da128415bf8b0124db
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-05-29 17:28:05 -0600

    Time: 0 ms (100%), Space: 42.7 MB (29.7%) - LeetHub

--- 1791-richest-customer-wealth/1791-richest-customer-wealth.cs
+++ 1791-richest-customer-wealth/1791-richest-customer-wealth.cs
@@ -1,13 +1,19 @@
 public class Solution {
     public int MaximumWealth(int[][] accounts) {
         int max = 0;
-        for(int i=0; i< accounts.Length;i++){
+        foreach(var customer in accounts){
             int currentMax = 0;
-            for(int j=0; j< accounts[i].Length;j++){
-                currentMax += accounts[i][j];
+            foreach(var account in customer){
+                currentMax += account;
             }
-            if(currentMax > max) max= currentMax;
-        }  
+            max = Math.Max(currentMax, max );
+        } 
         return max;      
     }
 }
+
+// The goeal is to find the customer with the highest wealth
+// Since Each row represnt a cutomer, I'll iterate over each customer and sum up thier account balances/ 
+
+// We want to track the the highest sum encountered -> variable max 
+// Update it whenever i find a wealthier customer
