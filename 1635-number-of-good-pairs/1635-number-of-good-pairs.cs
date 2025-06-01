f3577b591bafffc170000ff84dd65c69202ba23c
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-06-01 17:17:47 -0600

    Time: 0 ms (100%), Space: 41.2 MB (60.14%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cs
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cs
@@ -1,13 +1,18 @@
 public class Solution {
     public int NumIdenticalPairs(int[] nums) {
-
+        
+        Dictionary<int, int> Freq = new Dictionary<int, int>(); 
         int count = 0;
         for(int i =0; i< nums.Length; i++){
-            for(int j =0; j<nums.Length; j++){
-                if(nums[i] == nums[j] & i<j ) count++;
+            if(Freq.ContainsKey(nums[i])){
+                count += Freq[nums[i]];
+                Freq[nums[i]]++;
+            }else{
+                Freq[nums[i]] = 1;
             }
+            
         }
         return count;
-        
+        // Time: O(n^2)    
     }
 }
