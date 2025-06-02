21ed7fe5ea09e0692340ba7ebb376c0031990074
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-06-02 17:52:01 -0600

    Time: 0 ms (100%), Space: 41.2 MB (29.47%) - LeetHub

--- 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cs
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.cs
@@ -1,16 +1,25 @@
 public class Solution {
     public bool CheckIfPangram(string sentence) {
-        
-        Dictionary<char, int> seen = new Dictionary<char, int>();
-        int count = 0;
-        foreach(var str in sentence){
-            // Check if the element is already counted.
-            if(!seen.ContainsKey(str)) {
-                // If seen for the first time
-                count++;   // increase the count 
-                seen[str] = 1; // add it to dic
-            }
+
+        HashSet<char> uniqueLetters = new HashSet<char>();
+
+        foreach( char c in sentence){
+            uniqueLetters.Add(c);
+            if(uniqueLetters.Count == 26) return true;
         }
-        return count == 26;
+        return false;
+
+        // V01
+        // Dictionary<char, int> seen = new Dictionary<char, int>();
+        // int count = 0;
+        // foreach(var str in sentence){
+        //     // Check if the element is already counted.
+        //     if(!seen.ContainsKey(str)) {
+        //         // If seen for the first time
+        //         count++;   // increase the count 
+        //         seen[str] = 1; // add it to dic
+        //     }
+        // }
+        // return count == 26;
     }
 }
