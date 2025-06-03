41ae399666b08ccc638d042f934c0eca90bfaa2f
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-06-03 15:42:25 -0600

    Time: 2 ms (88.16%), Space: 57.1 MB (9.21%) - LeetHub

---
+++ 1899-count-items-matching-a-rule/1899-count-items-matching-a-rule.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue) {
+
+       int count = 0;
+       int ruleIndex = ruleKey switch{
+            "type" => 0,
+            "color" => 1,
+            _ => 2
+        };
+        foreach(var item in items){
+            if(item[ruleIndex] == ruleValue) count++;
+        }
+        return count;        
+    }
+}
