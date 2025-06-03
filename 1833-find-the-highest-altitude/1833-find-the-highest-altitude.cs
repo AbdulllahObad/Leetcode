df193a4249d2e53aba1c17e4934253109b5d861e
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-06-03 17:07:12 -0600

    Time: 0 ms (100%), Space: 41.9 MB (10.42%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cs
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cs
@@ -4,9 +4,15 @@ public class Solution {
         int currentAltitude = 0;
         int maxAltitude = 0;
         foreach(var change in gain){
-             maxAltitude =  Math.Max(maxAltitude, change + currentAltitude);
-             currentAltitude = change + currentAltitude;
+            // At each step, we add or loss to our current altitude.
+             maxAltitude =  Math.Max(maxAltitude, change + currentAltitude);// Update the max altitiude if we go higher
+             currentAltitude = change + currentAltitude;// Keep track of the current altitude
         }
+        //  [-5,1] -> max is 0, current 0
+        // first step:  we lost -5, at this point our max is 0 
+        // our current state now is -5, 
+        // second step: we gained 1 from our current, our max still the 0 we moved from at the begining, 
+        // our current altitude is -4, cause we were at -5 and we  moved on step up. 
         return maxAltitude;
         
     }
