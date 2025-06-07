6ce7e6b2ad0837784a6838cdfd93d1278c23c581
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-06-06 20:08:09 -0600

    Time: 0 ms (100%), Space: 47.9 MB (33.33%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.cs
+++ 0861-flipping-an-image/0861-flipping-an-image.cs
@@ -4,21 +4,25 @@ public class Solution {
         // Loop over each row.
         // 1. filp it
         // 2. Invert it
-        int [][] res = new int[image.Length][]; 
+        int [][] result = new int[image.Length][]; 
         for(int i =0; i< image.Length; i++){
-            res[i] = new int[image[i].Length];
-            int resIndex = 0;
-            for(int j = image[i].Length -1; j>-1; j--){
-                // Get the last element from image and filp it 
-                int reversedPix = image[i][j] ==0? 1: 0;
-                // Store it at incresed index in res: ex 0, 1, 2
-                res[i][resIndex] = reversedPix;
-                resIndex++;
+            int cols = image[i].Length;
+            result[i] = new int[image[i].Length];        
+            for(int j =0; j< cols; j++){
+                // Flip and invert in one step: 
+                result[i][j] = image[i][cols -1 -j]^1;
+
             }
         }
-
-        return res;
-
-        
+        return result;
     }
 }
+
+// V01:   
+
+// for(int j = image[i].Length -1; j>-1; j--){
+                // // Get the last element from image and filp it 
+                // int reversedPix = image[i][j] ==0? 1: 0;
+                // // Store it at incresed index in res: ex 0, 1, 2
+                // res[i][resIndex] = reversedPix;
+                // resIndex++;
