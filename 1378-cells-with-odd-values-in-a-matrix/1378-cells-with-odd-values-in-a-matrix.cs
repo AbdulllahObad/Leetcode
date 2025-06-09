8b2a801766824a691258f27e6bc8c60b54ba175d
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-06-09 02:19:39 -0600

    Time: 7 ms (7.41%), Space: 43.3 MB (7.41%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.cs
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.cs
@@ -5,22 +5,12 @@ public class Solution {
         int count = 0;
 
         // 1. Loop over the indices matrix 
-        for(int row =0; row < indices.Length; row++){   //O(n)
-             for(int col =0; col < indices[row].Length; col++){ // O(k)
-                // First elment 
-                if(col == 0){
-                    int r1 = indices[row][col]; // O(m)
-                    for(int i=0; i< n; i++){
-                         res[r1,i]++;
-                    }
-                }else{
-                    int c1 = indices[row][col]; // O(n)
-                    for(int i=0; i<m; i++){
-                        res[i,c1]++;
-                    }
-                }
-           
-            }
+        foreach (var index in indices) {
+                int row = index[0];
+                int col = index[1];             
+                for (int j = 0; j < n; j++) res[row, j]++;  // row increment
+                for (int i = 0; i < m; i++) res[i, col]++;  // col increment  
+            
         }
         return res.Cast<int>().Count(x => x % 2 != 0); // O(m * n)
 
