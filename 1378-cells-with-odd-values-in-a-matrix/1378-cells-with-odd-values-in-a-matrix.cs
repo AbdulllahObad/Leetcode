bbc4a0b23cb7ccea2b4e3e33c14ec6bd00e2a2d5
AbdulllahObad <abdullah.s.obad@gmail.com>
2025-06-09 01:26:38 -0600

    Time: 7 ms (7.41%), Space: 43.4 MB (7.41%) - LeetHub

--- 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.cs
+++ 1378-cells-with-odd-values-in-a-matrix/1378-cells-with-odd-values-in-a-matrix.cs
@@ -5,16 +5,16 @@ public class Solution {
         int count = 0;
 
         // 1. Loop over the indices matrix 
-        for(int row =0; row < indices.Length; row++){
-             for(int col =0; col < indices[row].Length; col++){
+        for(int row =0; row < indices.Length; row++){   //O(n)
+             for(int col =0; col < indices[row].Length; col++){ // O(k)
                 // First elment 
                 if(col == 0){
-                    int r1 = indices[row][col]; // 1
+                    int r1 = indices[row][col]; // O(m)
                     for(int i=0; i< n; i++){
                          res[r1,i]++;
                     }
                 }else{
-                    int c1 = indices[row][col];
+                    int c1 = indices[row][col]; // O(n)
                     for(int i=0; i<m; i++){
                         res[i,c1]++;
                     }
@@ -22,10 +22,9 @@ public class Solution {
            
             }
         }
-        return res.Cast<int>().Count(x => x % 2 != 0);
+        return res.Cast<int>().Count(x => x % 2 != 0); // O(m * n)
 
-        // 0 -> increment the res matrix in : res[1][0],res[1][1], res[1][2] 
-        // 1 -> inrement the res matrix in : res[0][c1], res[1][c1], res[2][c1]
+
 
 
 
